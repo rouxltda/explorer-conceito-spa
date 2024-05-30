7c20ca329de69d592d7e6ad68ea28065ca07ecbf
Ayres Roux <rouxltda@gmail.com>
2024-05-30 12:24:59 -0300

    mais conceitos de orientação a objetos

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
-const routes = {
-  "/": "/pages/home.html",
-  "/about": "/pages/about.html",
-  "/contact": "/pages/contact.html",
-  404: "/pages/404.html",
-};
+import { Router } from "./router.js";
 
-handle();
+const router = new Router();
 
-window.onpopstate = () => handle();
-window.route = () => route();
+router.add("/", "/pages/home.html");
+router.add("/about", "/pages/about.html");
+router.add("/contact", "/pages/contact.html");
+router.add(404, "/pages/404.html");
+
+router.handle();
+
+window.onpopstate = () => router.handle();
+window.route = () => router.route();
